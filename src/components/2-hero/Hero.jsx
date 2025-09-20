23717c165594d00fbcc749e487c761ecc3d4ff06
EngNody <96597324+EngNody@users.noreply.github.com>
2025-09-20 22:11:24 +0300

    hero

--- src/components/2-hero/Hero.jsx
+++ src/components/2-hero/Hero.jsx
@@ -4,8 +4,38 @@ import './hero.css'
 
 export default function Hero() {
   return (
-    <div>
-      HERO
+    <section className='hero flex' >
+      <div className='left-section'>
+        
+        <div className="parent-avatar flex">
+          <img src="/myimg.png" alt="" />
+          <div className='icon-verified'/>
     </div>
+
+
+
+    <div>
+      
+          <h1 className='title'>Software designer, founder, and amateur astronaut.</h1>
+          <p className='suptitle'>I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.</p>
+      
+         <div className="all-icons flex">
+          <div className="icon  icon-x"></div>
+          <div className="icon  icon-instagram"></div>
+          <div className="icon  icon-github"></div>
+          <div className="icon  icon-linkedin-square"></div>
+         </div>
+      
+      
+    </div>
+
+      </div>
+
+      <div className='right-section animation border'>
+       uuuuuuuuuuu
+      </div>
+
+
+    </section>
   )
 }
