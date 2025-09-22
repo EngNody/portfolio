7169ad5545ec8e56c0ea6a8bbfa4639919195c66
EngNody <96597324+EngNody@users.noreply.github.com>
2025-09-22 04:02:24 +0300

    contact-us

--- src/components/4-contact/Contact.jsx
+++ src/components/4-contact/Contact.jsx
@@ -4,8 +4,36 @@ import './contact.css'
 
 export default function Contact() {
   return (
-    <div>
-      CONTACT
-    </div>
+    <section className='contact-us'>
+        <h1 className='title'>   
+          <span className='icon-envelope'></span> 
+          Contact us
+       </h1>
+
+       <p className='sub-title'>Contact us for more information and Get notified when i publish something new</p>
+
+       <div className="flex">
+
+        <form action="" className="border">
+
+       <div className="flex">
+           <label htmlFor="email">Email Address:</label>
+           <input required type="email" id='email' placeholder='Your Name' />
+        
+       </div>
+
+       <div className='flex' style={{marginTop: '24px'}}>
+  
+            <label htmlFor="Message">your Message:</label>
+            <textarea required name="" id="Message" placeholder='Your message:'></textarea> 
+  
+       </div>
+          <button className='submit'>Submit</button>
+        </form>
+
+
+        <div className="border animation">animation</div>
+       </div>
+    </section>
   )
 }
