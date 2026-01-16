8d1c645842d328a1e4548860acc561aa513c4f88
EngNody <96597324+EngNody@users.noreply.github.com>
2026-01-16 19:46:00 +0200

    .

--- src/components/3-main/MyProjects.js
+++ src/components/3-main/MyProjects.js
@@ -1,6 +1,7 @@
 export const myProjects=[
 {  projectTitle:"Ecommerce project" , category:["react","pootstrap"], href:"https://ecommerce-76ae5.web.app",githuplink:"https://github.com/EngNody/E-commerce-project.git",imgPath:"/my-projects/Ecommerce-project.jpg",subtitle:""},
 {  projectTitle:"Admin dashboard" , category:["react","nivo"], href:"https://admin-dashboard-45d2e.web.app",githuplink:"https://github.com/EngNody/ADMIN-DASHBOARD.git",imgPath:"/my-projects/admin.png",subtitle:" a COMPLETE React Admin Dashboard App | React, Material UI, Data Grid, Light & Dark Mode"},
+{  projectTitle:"css project" , category:["css","aaa","java"],href:"https://my-online-shop112233.web.app/",githuplink:"https://github.com/EngNody/E-commerce-JS-project.git",imgPath:"/my-projects/online-shop.png",subtitle:""},
 {  projectTitle:"css project" , category:["css","aaa"],imgPath:"./React portfolio website/2.jpg",subtitle:""},
 {  projectTitle:"arrow project" , category:["react","uuu"],imgPath:"./React portfolio website/3.jpg",subtitle:""},
 {  projectTitle:"animation project" , category:["css"],imgPath:"./React portfolio website/4.jpg",subtitle:""},
